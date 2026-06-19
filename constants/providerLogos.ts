52f9307a6495a457864dfe05b4dc287d3659ae53
aynuel89@gmail.com <aynuel89@gmail.com>
2026-06-19 02:16:54 +0100

    VAS edits

---
+++ constants/providerLogos.ts
@@ -0,0 +1,34 @@
+// constants/providerLogos.ts
+
+export const providerLogos = {
+  mtn: require("@/assets/images/MTN-Logo.png"),
+  airtel: require("@/assets/images/Airtel_logo.png"),
+  glo: require("@/assets/images/glo.png"),
+  "9mobile": require("@/assets/images/9mobile.jpeg"),
+
+  "mtn-data": require("@/assets/images/MTN-Logo.png"),
+  "airtel-data": require("@/assets/images/Airtel_logo.png"),
+  "glo-data": require("@/assets/images/glo.png"),
+  "9mobile-data": require("@/assets/images/9mobile.jpeg"),
+
+  dstv: require("@/assets/images/dstv_nigeria.svg"),
+  gotv: require("@/assets/images/gotv.png"),
+  startimes: require("@/assets/images/Startimes.jpg"),
+  showmax: require("@/assets/images/Showmax_Logo.png"),
+
+  "smile-direct": require("@/assets/images/smile-logo.png"),
+  spectranet: require("@/assets/images/Spectranet.png"),
+
+  "eko-electric": require("@/assets/images/logo.png"),
+  "ikeja-electric": require("@/assets/images/Ikeja-Electric-Logo.png"),
+  "abuja-electric": require("@/assets/images/Aedc.jpeg"),
+  "kano-electric": require("@/assets/images/KEDCO.png"),
+  "port-harcourt-electric": require("@/assets/images/PHED.jpeg"),
+  "jos-electric": require("@/assets/images/jed-logo.png"),
+  "ibadan-electric": require("@/assets/images/IBEDC.png"),
+  "kaduna-electric": require("@/assets/images/kaedco.jpeg"),
+  "enugu-electric": require("@/assets/images/EEDC.png"),
+  "benin-electric": require("@/assets/images/bedc.png"),
+  "aba-electric": require("@/assets/images/aepl.png"),
+  "yola-electric": require("@/assets/images/yedc.png"),
+};
